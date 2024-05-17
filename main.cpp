6bb667dcec39482df1661ac5141c409e4c9e9c59
Rachel <lhearachel@proton.me>
2024-05-17 15:43:56 -0700

    Use subcommands instead of -p/-u switch

--- main.cpp
+++ main.cpp
@@ -49,68 +49,91 @@ int main(int argc, char *argv[])
     argparse::ArgumentParser program(PROGRAM_NAME, PROGRAM_VERSION);
     program.add_description("Utility for un/packing Nitro Archives for the Nintendo DS");
 
-    program.add_argument("-d", "--directory")
-        .metavar("DIRECTORY")
-        .help("directory to be packed or to dump unpacked files")
-        .store_into(directory);
-
-    auto &pack_or_unpack = program.add_mutually_exclusive_group(false);
-    pack_or_unpack.add_argument("-p", "--pack")
-        .metavar("TARGET")
-        .help("name of a NARC to be packed from DIRECTORY")
-        .store_into(target);
-    pack_or_unpack.add_argument("-u", "--unpack")
-        .metavar("SOURCE")
-        .help("name of a NARC to be unpacked into DIRECTORY")
-        .store_into(source);
-
-    program.add_argument("-f", "--filename-table")
-        .help("build the filename table")
+    // knarc pack
+    argparse::ArgumentParser pack_command("pack");
+    pack_command.add_description("Pack a list of files or a directory into a NARC");
+    pack_command.add_argument("-f", "--filename-table")
+        .help("build a filename table")
         .flag()
         .store_into(pack_with_fnt);
-
-    program.add_argument("-n", "--naix")
+    pack_command.add_argument("-n", "--naix")
         .help("output a C-style .naix header")
         .flag()
         .store_into(output_header);
-
-    program.add_argument("--prefix-header-entries")
+    pack_command.add_argument("--prefix-header-entries")
         .help("prefix entries in an output .naix header with TARGET; dependent on --naix")
         .flag()
         .store_into(prefix_header_entries);
+    pack_command.add_argument("-z", "--version-zero")
+        .help("output the NARC as version 0 spec")
+        .flag()
+        .store_into(use_v0);
 
-    program.add_argument("-i", "--ignore")
+    // knarc pack files
+    argparse::ArgumentParser pack_files_command("files");
+    pack_files_command.add_description("Pack a list of files into NARC");
+    pack_files_command.add_argument("target")
+        .metavar("NARC")
+        .help("name of the packed NARC")
+        .required()
+        .store_into(target);
+    pack_files_command.add_argument("files")
+        .metavar("FILES")
+        .help("list of one or more files to be packed")
+        .remaining()
+        .nargs(argparse::nargs_pattern::at_least_one)
+        .store_into(input_files);
+    pack_command.add_subparser(pack_files_command);
+
+    // knarc pack dir
+    argparse::ArgumentParser pack_dir_command("dir");
+    pack_dir_command.add_description("Pack DIRECTORY's contents into NARC");
+    pack_dir_command.add_argument("target")
+        .metavar("NARC")
+        .help("name of the packed NARC")
+        .required()
+        .store_into(target);
+    pack_dir_command.add_argument("directory")
+        .metavar("DIRECTORY")
+        .help("directory to be packed")
+        .required()
+        .store_into(directory);
+    pack_dir_command.add_argument("-i", "--ignore")
         .metavar("IGNORE_FILE")
         .help("specify a file listing file-patterns to ignore for packing")
         .store_into(ignore_fname);
-
-    program.add_argument("-k", "--keep")
+    pack_dir_command.add_argument("-k", "--keep")
         .metavar("KEEP_FILE")
         .help("specify a file listing file-patterns to keep during packing; "
               "listed patterns override those matching patterns in IGNORE_FILE")
         .store_into(keep_fname);
-
-    program.add_argument("-o", "--order")
+    pack_dir_command.add_argument("-o", "--order")
         .metavar("ORDER_FILE")
         .help("specify a file listing order of files for packing; "
               "listed files override those matching patterns in IGNORE_FILE")
         .store_into(order_fname);
+    pack_command.add_subparser(pack_dir_command);
 
-    program.add_argument("-z", "--version-zero")
-        .help("output the NARC as version 0 spec")
-        .flag()
-        .store_into(use_v0);
+    // knarc unpack
+    argparse::ArgumentParser unpack_command("unpack");
+    unpack_command.add_description("Unpack a NARC into DIRECTORY");
+    unpack_command.add_argument("source")
+        .metavar("NARC")
+        .help("name of the NARC to unpack")
+        .required()
+        .store_into(source);
+    unpack_command.add_argument("directory")
+        .metavar("DIRECTORY")
+        .help("parent directory for unpacked files")
+        .required()
+        .store_into(directory);
 
     program.add_argument("--verbose")
         .help("output additional program messages")
         .flag()
         .store_into(debug);
-
-    program.add_argument("files")
-        .metavar("FILES")
-        .help("list of files to pack into the destination NARC; if specified, -d, -i, -k, and -o will be ignored")
-        .nargs(argparse::nargs_pattern::any)
-        .store_into(input_files);
+    program.add_subparser(pack_command);
+    program.add_subparser(unpack_command);
 
     if (argc > 1 && argv[1][0] == '@') {
         response_file_args.push_back(argv[0]);
@@ -141,20 +164,19 @@ int main(int argc, char *argv[])
         std::cout << "[DEBUG] ignore file: " << ignore_fname << std::endl;
         std::cout << "[DEBUG] keep file:   " << keep_fname << std::endl;
         std::cout << "[DEBUG] order file:  " << order_fname << std::endl;
+
+        std::cout << "[DEBUG] input files: [ ";
+        for (auto &entry : input_files) {
+            std::cout << entry << ", ";
+        }
+        std::cout << "]" << std::endl;
     }
 
-    narc::NarcError err;
+    narc::NarcError err = narc::NarcError::None;
     if (source.empty()) {
         if (input_files.empty()) {
-            if (directory.empty()) {
-                std::cerr << "-d: required" << std::endl;
-                std::cerr << program << std::endl;
-                std::exit(1);
-            }
-
             err = narc::pack(target, directory, order_fname, ignore_fname, keep_fname);
         } else {
-            std::cout << "packing from input files" << std::endl;
             err = narc::pack(target, input_files);
         }
     } else {
